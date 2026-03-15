1dd3bb738ca27aa99d025f72a0a8fa0338a54ec3
ajalali <ariel.jalali@gmail.com>
2026-03-15 14:13:32 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useRef, useCallback, useEffect } from 'react'
+import { useState, useRef, useCallback } from 'react'
 import Image from 'next/image'
 
 const MAX_HISTORY = 6
